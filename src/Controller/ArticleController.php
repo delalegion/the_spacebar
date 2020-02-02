dc1efdf97e86e566e674ffbe2b34d6d43b54d29e
delalegion <nielotweb@gmail.com>
2020-02-02 17:15:22 +0100

    Update files 02-02

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,11 +2,11 @@
 
 namespace App\Controller;
 
-
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -15,11 +15,14 @@ class ArticleController
     {
         return new Response('OMG! MY first homepage already is ddasone!');
     }
+
     /**
      * @Route("/news/{slug}")
      */
     public function show($slug)
     {
-        return new Response(sprintf('Monster? How it does likes?! %s', $slug));
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+        ]);
     }
 }
